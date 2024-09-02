be2fae79701ef8118222198f4eb1838d2fe8f2fc
User Den <Vegan585@yandex.ru>
2024-09-02 23:51:14 +0300

    Выполнение Т3_7.2, новый тест, отдельный класс для CSV, учтены замечания

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -6,10 +6,10 @@ public abstract class Managers {
     public static FileBackedTaskManager getDefaultFileManager(File file) {
         return FileBackedTaskManager.loadFromFile(file);
     }
+
     public static TaskManager getDefault(File file) {
         return new FileBackedTaskManager(file);
     }
-
 
     static InMemoryHistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
